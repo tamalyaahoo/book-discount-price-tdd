f940296b294ccdc085f2d7034c5d5435cbd191bf
TAMAL NAYEK <tamal.nayek@yahoo.com>
2025-11-30 16:58:35 +0530

    feat: Add new other test case to handle more books discount.
    Implemented another test case, to check 4 books & 5 books discount 20% and 25% respectively. calculate total discounted price.
    
    Closes #4

--- src/test/java/com/bnpp/kata/bookdiscount/app/service/BookDiscountServiceTest.java
+++ src/test/java/com/bnpp/kata/bookdiscount/app/service/BookDiscountServiceTest.java
@@ -48,4 +48,31 @@ class BookDiscountServiceTest {
         assertEquals(135.0, service.calculatePrice(items), 0.01);
     }
 
+    @Test
+    @DisplayName("Apply 20% discount for four different books")
+    void testFourDifferentBooks_20PercentDiscount() {
+        List<Book> items = List.of(
+                new Book("Clean Code", 1),
+                new Book("The Clean Coder", 1),
+                new Book("Clean Architecture", 1),
+                new Book("TDD", 1)
+        );
+
+        assertEquals(160.0, service.calculatePrice(items), 0.01);
+    }
+
+    @Test
+    @DisplayName("Apply 25% discount for all five different books")
+    void testAllFiveBooks_25PercentDiscount() {
+        List<Book> items = List.of(
+                new Book("Clean Code", 1),
+                new Book("The Clean Coder", 1),
+                new Book("Clean Architecture", 1),
+                new Book("TDD", 1),
+                new Book("Legacy Code", 1)
+        );
+
+        assertEquals(187.50, service.calculatePrice(items), 0.01);
+    }
+
 }
